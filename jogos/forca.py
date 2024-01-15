c42e12e053a089c1037453590823d41102b68c75
Alvaro Carvalho <alvarocarv2004@gmail.com>
2024-01-14 21:41:53 -0300

    Atualizando os arquivos do jogo da forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,7 +3,20 @@ def jogar():
     print("    JOGO DA FORCA    ")
     print("*********************")
 
-    print("Fim de jogo!")
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+        chute = input("Escolha uma letra: ")
+        chute = chute.strip()
+
+        contador = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("A letra {} foi encontrada na posição: {}".format(chute.upper(), contador))
+            contador += 1
+        print("jogando...")
 
 if __name__ == "__main__":
     jogar()
