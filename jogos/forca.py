f730efa25f9b0438637710c4b1a7eb9dd08e9614
Alvaro Carvalho <alvarocarv2004@gmail.com>
2024-01-26 14:19:27 -0300

    exibir a palavra da forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,9 +5,11 @@ def jogar():
 
     palavra_secreta = "banana"
     enforcou = False
-    acertou = False
 
-    while (not enforcou and not acertou):
+    lista_acertada = ['_', '_', '_', '_', '_', '_']
+    print(lista_acertada)
+
+    while ('_' in lista_acertada):
         chute = input("Escolha uma letra: ")
         chute = chute.strip()
 
@@ -15,8 +17,9 @@ def jogar():
         for letra in palavra_secreta:
             if chute.upper() == letra.upper():
                 print("A letra {} foi encontrada na posição: {}".format(chute.upper(), contador))
+                lista_acertada[contador] = chute.lower()
             contador += 1
-        print("jogando...")
+        print(lista_acertada)
 
 if __name__ == "__main__":
     jogar()
